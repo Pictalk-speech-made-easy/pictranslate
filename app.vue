5b47a4ef16153082478b111418535024b42ee4b9
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-10-09 16:38:13 +0200

    Added charset

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
   <head>
     <link rel="manifest" href="/manifest.webmanifest">
     <meta name="theme-color" content="#8296FF">
+    <meta name="charset" content="utf-8">
   </head>
   <VitePwaManifest />
     <NuxtLayout>
