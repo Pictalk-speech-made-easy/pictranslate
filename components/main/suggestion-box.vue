34d3166603afd29c62e2a07288bcf6731caa9a1c
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-10-19 13:54:15 +0200

    Added images to each suggestion

--- components/main/suggestion-box.vue
+++ components/main/suggestion-box.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="flex flex-wrap mx-1 p-1 items-center">
         <div v-for="pictogram in pictograms" class="!m-0 object-contain h-12">
-            <button @click="emit('suggestion', pictogram)" class="btn btn-primary">{{ pictogram['keywords'][locale][0]['keyword'] }}</button>
+            <button @click="emit('suggestion', pictogram)" class="btn btn-primary">
+              <span>{{ pictogram['keywords'][locale][0]['keyword'] }}</span>
+              <img class="!m-0 aspect-square object-contain h-8 rounded-sm zoom-in" :src="pictogram.external_alt_image.toString()"
+                        :alt="pictogram['keywords'][locale][0]['keyword']"/>
+            </button>
         </div>
     </div>
 </template>
