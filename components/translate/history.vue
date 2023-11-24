101aacddc47fa3a732761e377b085dda2189d8ff
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-11-24 16:42:26 +0100

    Hotfix history

--- components/translate/history.vue
+++ components/translate/history.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="mt-12" v-if="history.length > 0">
+  <div class="mt-12" v-if="historyDatabase.history.length > 0">
     <p class="ml-4 mb-2 text-2xl font-serif">{{ $t('main.history') }}</p>
     <btn @click="onHistoryClick(historyItem)"
       class="btn h-auto mx-4 my-2 px-2 py-1 justify-start rounded-lg shadow-md bg-blue-100 dark:bg-blue-900"
-      v-for="(historyItem, index) in history">
+      v-for="(historyItem, index) in historyDatabase.history">
       <div class="flex justify-start p-1 pb-0 overflow-x-auto">
         <btn class="max-w-[15%]" tabindex="0" v-for="(pictogramPropositions, index) in historyItem.pictogramsPropositions">
           <img crossorigin="anonymous" class="!m-0 aspect-square object-contain rounded-sm zoom-in"
@@ -21,11 +21,11 @@ import { useHistoryDatabase } from '~/store/history';
 import { HistoryItem } from '~/store/store-types';
 const options = useOptions();
 const historyDatabase = useHistoryDatabase();
-const { history } = historyDatabase;
 const main = useMain();
 
-onMounted(() => {
-  historyDatabase.getHistory();
+onMounted(async () => {
+  await historyDatabase.getHistory();
+  console.debug("[history]", historyDatabase.history)
 })
 
 const onHistoryClick = function (record: HistoryItem) {
