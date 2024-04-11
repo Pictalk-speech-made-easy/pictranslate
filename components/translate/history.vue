6986826b4576723d733b521d891570074042110f
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2024-04-11 09:50:43 +0200

    Beautiful history

--- components/translate/history.vue
+++ components/translate/history.vue
@@ -2,16 +2,16 @@
   <div class="mt-12" v-if="historyDatabase.history.length > 0">
     <p class="ml-4 mb-2 text-2xl font-serif">{{ $t('main.history') }}</p>
     <button @click="onHistoryClick(historyItem)"
-      class="btn h-auto mx-4 my-2 px-2 py-1 justify-start rounded-lg shadow-md bg-blue-100 dark:bg-blue-900"
+      class="btn w-full h-auto mx-4 my-2 px-2 py-1 block rounded-lg shadow-md bg-blue-100 dark:bg-blue-900"
       v-for="(historyItem, index) in historyDatabase.history">
-      <div class="flex justify-start p-1 pb-0 overflow-x-auto">
+      <div class="flex justify-center p-1 pb-0 overflow-x-auto">
         <button class="max-w-[15%]" tabindex="0"
           v-for="(pictogramPropositions, index) in historyItem.pictogramsPropositions">
-          <img crossorigin="anonymous" class="!m-0 aspect-square object-contain rounded-sm zoom-in"
+          <img crossorigin="anonymous" class="!m-0 aspect-square object-contain rounded-sm zoom-in min-w-16"
             :src="getPictogram(pictogramPropositions)" :alt="getAlt(pictogramPropositions)" />
         </button>
       </div>
-      <p class="lowercase font-normal text-base ml-4 text-left dark:text-gray-300">{{ historyItem.text_input }}</p>
+      <p class="lowercase font-normal text-base text-center dark:text-gray-300">{{ historyItem.text_input }}</p>
     </button>
   </div>
 </template>
