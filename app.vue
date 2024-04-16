449e4807ac9c85979cc6c543a79904d991e7599f
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2024-04-16 10:44:25 +0200

    Added matomo tracker on the app.vue

--- app.vue
+++ app.vue
@@ -17,6 +17,13 @@ import { useGramDatabase } from './store/gram-db';
 import { useHistoryDatabase } from './store/history';
 import { useMiniPictohubDatabase } from './store/mini-pictohub-db';
 
+onMounted(() => {
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({ 'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start' });
+  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+  g.async = true; g.src = 'https://analytics.picmind.org/js/container_1XZHKffi.js'; s.parentNode.insertBefore(g, s);
+})
+
 // Initialize gram database
 const GramDatabase = useGramDatabase();
 await GramDatabase.initialize_database();
